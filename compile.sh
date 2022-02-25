0d5942606f2040876910cf6c58004bd6458e7e81
Maxime Oliva <maxime.oliva@atos.net>
2022-02-25 09:52:20 -0700

    Clean up code + test pass

--- compile.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# This script will disappear and will be replaced by a global script
-# written by Jerome (like "bldit")
-
-# Compile
-mkdir -p build
-cmake -B build -DCMAKE_INSTALL_PREFIX:PATH=$HOME/.local/qlm && cd build && make -j4 install
--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
-import os, sys
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+
+"""
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
+"""
+
+import sys
+from setuptools import setup, find_namespace_packages
 from setuptools.command.test import test as TestCommand
 
+
 class PyTest(TestCommand):
-    """
-    A test command to run pytest on a the full repository.
-    This means that any function name test_XXXX
-    or any class named TestXXXX will be found and run.
-    """
     def initialize_options(self):
         TestCommand.initialize_options(self)
         self.pytest_args = []
@@ -19,18 +34,39 @@ class PyTest(TestCommand):
 
     def run_tests(self):
         import pytest
-        errno = pytest.main([".", "-vv"])
+        errno = pytest.main([".", "-v"])
         sys.exit(errno)
 
-setup(
-    name="mylib",
-    version="0.0.1",
-    author="XXX",
-    license="European Union Public License 1.2",
 
-    packages=find_packages(),
-    install_requires=["numpy"],
-    # Don't change these two lines
-    tests_require=["pytest"],
+def get_description():
+    """
+    Returns the long description of the current
+    package
+    Returns:
+        str
+    """
+    with open("README.md", "r") as readme:
+        return readme.read()
+
+
+setup(
+    name="myqlm-fermion",
+    version="0.0.1",
+    author="Atos Quantum Lab",
+    license="Atos myQLM EULA",
+    description="myQLM-fermion package",
+    long_description=get_description(),
+    long_description_content_type='text/markdown',
+    url="https://atos.net/en/lp/myqlm",
+    project_urls={
+        "Documentation": "https://myqlm.github.io",
+        "Bug Tracker": "https://github.com/myQLM/myqlm-issues/issues",
+        "Community": "https://myqlmworkspace.slack.com",
+        "Source code": "https://github.com/myQLM/myqlm-fermion"
+    },
+    packages=find_namespace_packages(include=["qat.*"]),
+    test_suite="tests",
+    install_requires=["qat-core", "qat-lang"],
+    tests_require=['pytest'],
     cmdclass={'test': PyTest},
 )
