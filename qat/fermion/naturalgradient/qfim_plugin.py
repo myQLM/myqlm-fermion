271118271cc810301f2a2d1db53d9f9342f30bcd
Oliva, Maxime <maxime.oliva@atos.net>
2022-07-04 15:06:23 +0200

    Update Natural graident plugin

--- qat/fermion/naturalgradient/qfim_plugin.py
+++ qat/fermion/naturalgradient/qfim_plugin.py
@@ -37,17 +37,17 @@ class GradientMinimizePlugin(Junction):
     For regular gradient descent, we choose :math:`g = I`.
 
     Args:
-        maxiter (int, optional): maximum number of iterations.
+        maxiter (int, optional): Maximum number of iterations.
             Defaults to 1000.
         natural_gradient (bool, optional) : Whether to perform natural gradient descent or "classical"
             gradient descent. Default to True.
         lambda_step (float, optional): Gradient descent step size :math:`\lambda`.
             Defaults to 0.2.
-        stop_crit (string, optional): stopping criterion, among {"grad_norm"|"energy_dist"}.
+        stop_crit (string, optional): Stopping criterion, among {"grad_norm"|"energy_dist"}.
             Defaults to grad_norm.
-        tol (float, optional): tolerance for stopping criterion.
+        tol (float, optional): Tolerance for stopping criterion.
             Defaults to 1e-10.
-        x0 (dict, optional): initial value of the parameters. If None, the initial parameters will be randomly chosen. Defaults to
+        x0 (dict, optional): Initial value of the parameters. If None, the initial parameters will be randomly chosen. Defaults to
             None.
 
     """
@@ -57,19 +57,14 @@ class GradientMinimizePlugin(Junction):
         maxiter: int = 1000,
         lambda_step: float = 0.2,
         natural_gradient: bool = True,
-        do_compute_energy: bool = True,
         stop_crit: str = "grad_norm",
         tol: float = 1e-10,
         x0: Dict[str, float] = None,
         user_custom_gates=None,
     ):
 
-        if do_compute_energy == False and stop_crit == "energy_dist":
-            raise PluginException(f"Cannot check energy update steps if 'do_compute_energy' is set to {do_compute_energy}")
-
         self.lambda_step = lambda_step
         self.natural_gradient = natural_gradient
-        self.do_compute_energy = do_compute_energy
 
         # Parameters values
         self.parameters_index = {}
@@ -143,21 +138,15 @@ class GradientMinimizePlugin(Junction):
 
         nb_parameters = len(self.parameters_values)
 
-        energy_trace = None
-
-        if self.do_compute_energy:
-            energy_trace = []
-
+        energy_trace = []
         angle_trace = []
 
         angle_trace.append([val for val in self.parameters_values.values()])  # Assumes dictionaries are ordered
 
-        if self.do_compute_energy:
+        pristine_job = qlm_object(gate_set=self.my_gate_set, **self.parameters_values)
 
-            pristine_job = qlm_object(gate_set=self.my_gate_set, **self.parameters_values)
-
-            energy = self.execute(pristine_job).value
-            energy_trace.append(energy)
+        energy = self.execute(pristine_job).value
+        energy_trace.append(energy)
 
         gradient_jobs_dict = auto_differentiation_gradient_dictionary(
             job, hamilt, self.parameters_values, user_custom_gates=self.custom_gates
@@ -182,9 +171,9 @@ class GradientMinimizePlugin(Junction):
                 val_list = self.execute_weighted_jobs_list(jobs_list)
                 q_gradient[var_key] = sum(val_list)
 
+            # Correct gradient with the Quantum Fisher Information Matrix as a metric tensor
             if self.natural_gradient:
 
-                # The plugin was asked to correct gradient with the Quantum Fisher Information Matrix as a metric tensor
                 QFIM = np.zeros((nb_parameters, nb_parameters), dtype=float)
 
                 for k in range(nb_parameters):
@@ -208,19 +197,19 @@ class GradientMinimizePlugin(Junction):
 
                         QFIM[k][l] = 4 * (dkdl - dkpsi * psidl)
 
-                # Get rid of too small values
+                # Truncate zero values
                 QFIM = np.where(np.abs(QFIM) < 1e-10, 0.0, QFIM)
 
                 # Inverse the metric tensor
                 invQFIM = np.linalg.pinv(QFIM)
 
-            # Perform optimisation step (update parameters)
+            # Perform optimization step (update parameters)
             # $\vec{\vartheta} <- \vec{\vartheta} - \lambda_step * [QFIM]^{-1} * \nabla E$
 
             cur_params = np.array([p_val for p_val in self.parameters_values.values()])
             cur_grad = np.array([grad_i for grad_i in q_gradient.values()])
 
-            # Get rid of too small values
+            # Truncate zero values
             cur_grad = np.real(np.where(np.abs(cur_grad) < 1e-10, 0.0, cur_grad))
 
             if self.natural_gradient:
@@ -239,19 +228,17 @@ class GradientMinimizePlugin(Junction):
                 self.parameters_values[self.parameters_index[ind_param]] = cur_params[ind_param]
 
             # Update Optimization Data
-            angle_trace.append([ang for ang in self.parameters_values.values()])  # Assumes dictionaries are ordered...
+            angle_trace.append([ang for ang in self.parameters_values.values()])  # Assumes dictionaries are ordered
 
-            if self.do_compute_energy:
+            # Temporary for XX gate
+            binded_job = qlm_object(gate_set=self.my_gate_set, **self.parameters_values)
 
-                # Temporary for XX gate
-                binded_job = qlm_object(gate_set=self.my_gate_set, **self.parameters_values)
+            energy = self.execute(binded_job).value
 
-                energy = self.execute(binded_job).value
+            if self.stop_crit == "energy_dist" and self.iterations > 0:
+                self.check_crit_val = np.abs(energy - energy_trace[-1])
 
-                if self.stop_crit == "energy_dist" and self.iterations > 0:
-                    self.check_crit_val = np.abs(energy - energy_trace[-1])
-
-                energy_trace.append(energy)
+            energy_trace.append(energy)
 
             if self.stop_crit == "grad_norm":
                 self.check_crit_val = np.linalg.norm(cur_grad)
