65e9db1d0eea08183ce36399b0248ae5280b5c60
Maxime Oliva <100301276+moliva7@users.noreply.github.com>
2023-02-21 17:32:38 +0100

    Make module compatible with dynamic module import

--- qat/__init__.py
+++ qat/__init__.py
@@ -5,5 +5,9 @@ Init
 
 # Try to find other packages in other folders (with separate build directory)
 from pkgutil import extend_path
-
 __path__ = extend_path(__path__, __name__)
+
+# Initialize dynamic modules
+from .modules import register_modules
+register_modules()
+
